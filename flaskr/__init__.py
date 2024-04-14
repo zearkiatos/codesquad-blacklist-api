6b896cd991629b9ceeb0fd3a6218066489a45895
zearkiatos <caprilespe@outlook.com>
2024-04-14 10:41:38 -0400

    Fixing error

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,17 +1,15 @@
 from flask import Flask
 from config import Config
 from datetime import timedelta
-import os
 
 
-DATABASE_URI = 
 def create_app(config_name):
     config = Config()
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] = config.JWT_SECRET_KEY
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=4)
     app.config['SECRET_KEY'] = config.SECRET_KEY
-    app.config['SQLALCHEMY_DATABASE_URI']=f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}:{os.environ['RDS_PORT']}/{os.environ['RDS_DB_NAME']}"#config.DATA_BASE_URI
+    app.config['SQLALCHEMY_DATABASE_URI']=config.DATA_BASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
     app.config['PROPAGATE_EXCEPTIONS']=True
     return app
